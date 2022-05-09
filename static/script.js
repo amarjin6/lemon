72db21478acdca29c747f21f2b2e66b257b240fd
Alexander Marjin <amarjin6@gmail.com>
2022-05-09 14:48:09 +0300

    Add Login validation

--- static/script.js
+++ static/script.js
@@ -3,10 +3,16 @@ $singUpBtn = document.getElementById('singUpButton');
 $passwordSingUp = document.getElementById('passwordSingUp');
 $passwordConfirm = document.getElementById('passwordConfirm');
 $usernameSingUp = document.getElementById('usernameSingUp');
+$emailLogin = document.getElementById('emailLogin');
+$passwordLogin = document.getElementById('passwordLogin');
+$buttonLogin = document.getElementById('buttonLogin');
 $singUpBtn.disabled = true;
+$buttonLogin.disabled = true;
 $email.addEventListener("change", stateHandle);
 $passwordSingUp.addEventListener("change", stateHandle1);
 $usernameSingUp.addEventListener("change", stateHandle2);
+$emailLogin.addEventListener("change", stateHandle3);
+$passwordLogin.addEventListener("change", stateHandle4);
 
 function stateHandle() {
     if (validateEmail($email.value)) {
@@ -31,6 +37,22 @@ function stateHandle2() {
         $singUpBtn.disabled = false;
     } else {
         $singUpBtn.disabled = true;
+    }
+}
+
+function stateHandle3() {
+    if (validateEmail($emailLogin.value)) {
+        $buttonLogin.disabled = false;
+    } else {
+        $buttonLogin.disabled = true;
+    }
+}
+
+function stateHandle4() {
+    if (validatePassword($passwordLogin.value)) {
+        $buttonLogin.disabled = false;
+    } else {
+        $buttonLogin.disabled = true;
     }
 }
 
